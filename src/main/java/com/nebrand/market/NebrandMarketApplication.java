6c8af8138ee7a8ebc7883d431be39dd307a31dda
Miguel Betancurth <mbetancurthb@gmail.com>
2024-09-10 15:37:43 -0500

    Se sube API para Super mercado

---
+++ src/main/java/com/nebrand/market/NebrandMarketApplication.java
@@ -0,0 +1,13 @@
+package com.nebrand.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebrandMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebrandMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebrand/market/NebrandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebrand.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebrandMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
